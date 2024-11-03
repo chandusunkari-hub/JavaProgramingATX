2a813f6a6ada55711dc4aaeb089a8aa0d0f09451
chandusunkari <ravichandrasunkari@gmail.com>
2024-11-03 19:20:06 +0530

    all the files added

---
+++ src/oct/ex_02102024/Lab0001.java
@@ -0,0 +1,12 @@
+package oct.ex_02102024;
+
+import javax.xml.transform.stream.StreamSource;
+import java.sql.SQLOutput;
+
+public class Lab0001 {
+    public static void main(String[]args){
+        System.out.println("Hello world ");
+        System.out.println("Good morning");
+        System.out.println("git");
+    }
+}
---
+++ src/oct/ex_03102024/Lab001.java
@@ -0,0 +1,4 @@
+package oct.ex_03102024;
+
+public class Lab001 {
+}
